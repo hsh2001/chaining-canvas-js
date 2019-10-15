3032c03b034d8594707b502d9465d9dcdb0ae3ee
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2019-10-15 10:56:31 +0900

    FIX comments.

--- core/core.js
+++ core/core.js
@@ -24,6 +24,56 @@ const imgConstructorList = [
 ];
 
 //  define pathMaker.
+/**
+*  @callback PathGenerator
+*  @param {Object} pathMaker
+*    @param {PathMoveTo} pathMaker.moveTo
+*      @callback PathMoveTo
+*      @param {Integer} [x=0]
+*      @param {Integer} [y=0]
+*    @param {PathLineTo} pathMaker.lineTo
+*      @callback PathLineTo
+*      @param {Integer} [x=0]
+*      @param {Integer} [y=0]
+*    @param {PathPoint} pathMaker.point
+*      @callback PathPoint
+*      @param {Integer} [x=0]
+*      @param {Integer} [y=0]
+*    @param {} pathMaker.arc
+*      @callback PathArc
+*      @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
+*      @param {Integer} [x=0]
+*      @param {Integer} [y=0]
+*      @param {Integer} [r=0]
+*      @param {Float} [sAngle=0]
+*      @param {Float} [eAngle=0]
+*      @param {Boolean} [counterclockwise=false]
+*    @param {} pathMaker.circle
+*      @callback PathCircle
+*      @param {Integer} [x=0]
+*      @param {Integer} [y=0]
+*      @param {Integer} [r=0]
+*    @param {} pathMaker.bezierCurveTo
+*      @callback PathBezierCurveTo
+*      @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/bezierCurveTo
+*      @param {Integer} [cp1x=0]
+*      @param {Integer} [cp1y=0]
+*      @param {Integer} [cp2x=0]
+*      @param {Integer} [cp2y=0]
+*      @param {Integer} [x=0]
+*      @param {Integer} [y=0]
+*    @param {} pathMaker.ellipse
+*      @callback PathEllipse
+*      @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/ellipse
+*      @param {Integer} [x=0]
+*      @param {Integer} [y=0]
+*      @param {Integer} [radiusX=0]
+*      @param {Integer} [radiusY=0]
+*      @param {Float} [rotation=0]
+*      @param {Float} [sAngle=0]
+*      @param {Float} [eAngle=0]
+*      @param {Boolean} [anticlockwise=false]
+*/
 const pathMaker = {};
 const pathMakerList = [
   ["moveTo", 2],
@@ -244,8 +294,13 @@ return class ChaningCanvas {
   /**
   *  @method
   *  @param {String} format (optional)
-  *  @param {Function} cb (optional)
+  *  @param {ImageEventHandler} cb (optional)
   *  @return {Promise} (When cb is not a function) resolve image object when process is done.
+  */
+  /**
+  *  @callback ImageEventHandler
+  *  @param {Image} img
+  *  @param {Error} error
   */
   toImg(...p) { return this.toImage(...p); }
   toImage(format, cb) {
@@ -315,7 +370,11 @@ return class ChaningCanvas {
   /**
   *  @method
   *  @param {Object} style (optional)
-  *  @param {Function} fn
+  *  @param {CanvasExecuter} fn
+  */
+  /**
+  *  @callback CanvasExecuter
+  *  @param {ChaningCanvas} canvas
   */
   execute(style, fn) {
     const ctx = this.ctx;
@@ -376,7 +435,7 @@ return class ChaningCanvas {
 
   /**
   *  @method
-  *  @param {Function} cb
+  *  @param {PathGenerator} cb
   */
   addPath(cb) {
     const ctx = this.ctx;
@@ -412,7 +471,7 @@ return class ChaningCanvas {
 
   /**
   *  @method
-  *  @param {Function} cb
+  *  @param {PathGenerator} cb
   */
   setPath(cb) {
     return this
